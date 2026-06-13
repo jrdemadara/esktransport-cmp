37b363f19dd1b52316edfdf987f087c305c28f91
jrdemadara <jrdemadara@protonmail.com>
2026-06-13 15:05:05 +0800

    fix: support dark mode system bars on Android

--- androidApp/src/main/kotlin/org/noztek/esktransport/MainActivity.kt
+++ androidApp/src/main/kotlin/org/noztek/esktransport/MainActivity.kt
@@ -1,16 +1,24 @@
 package org.noztek.esktransport
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import org.noztek.esktransport.app.di.createPlatformKoinContext
 import org.noztek.esktransport.app.di.initKoinPlatform
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val isDarkMode = resources.configuration.isSystemInDarkMode()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = !isDarkMode
+            isAppearanceLightNavigationBars = !isDarkMode
+        }
+
         initKoinPlatform(
             createPlatformKoinContext(
                 rawContext = applicationContext,
@@ -26,6 +34,10 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
+}
+
+private fun Configuration.isSystemInDarkMode(): Boolean {
+    return uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
 }
 
 @Preview
